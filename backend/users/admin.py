10ea522929ed38113ad4712e7a5d5b093ed3e9e5
anuvindreghu5-lab <anuvindreghu5@gmail.com>
2026-05-15 21:41:35 +0530

    fix: update users admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'user_type', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {
+            'fields': (
+                'user_type',
+                'phone_number',
+                'date_of_birth',
+                'bio',
+                'total_tests_taken',
+                'total_score',
+            )
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
