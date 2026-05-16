a0075895f801dd4b79f94c3fb037061d89e20d31
anuvindreghu5-lab <anuvindreghu5@gmail.com>
2026-05-17 00:37:10 +0530

    update backend url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,7 +132,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://astounding-truffle-6129f9.netlify.app"
+    "https://mock-test-exam.netlify.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
