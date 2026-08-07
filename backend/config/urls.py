0edcf136e4e45711f9ef356d4f906cf21ab66e33
anuvindreghu5-lab <anuvindreghu5@gmail.com>
2026-08-07 19:55:51 +0530

    Configure Django to explicitly serve media files in production (DEBUG=False) for Gunicorn hosting

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
@@ -28,5 +28,9 @@ urlpatterns = [
     ),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Explicitly serve media files in production (DEBUG=False) on Render
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
